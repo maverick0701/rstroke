c8c5a833958bdac4e13d34084961909fec256e0f
@maverick0701 <madhavanandcp070@gmail.com>
2020-11-26 19:02:12 +0530

    created create controller

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,4 +1,35 @@
+const User = require('../models/user');
+
 module.exports.home=function(req,res){
     return res.render('_homePage.ejs');
 }
 
+module.exports.display=function(req,res)
+{
+    return res.render('_secondPage.ejs');
+}
+
+
+module.exports.create = function(req, res){
+    if (req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){req.flash('error', err); return}
+
+        if (!user){
+            User.create(req.body, function(err, user){
+                if(err){
+                    console.log(error,"at line 26");
+                    return;
+                }
+
+                return res.redirect('/');
+            })
+        }else{
+            return res.redirect('back');
+        }
+
+    });
+}
