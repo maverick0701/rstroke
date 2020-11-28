129f79b1f9165d77229eadaebeb95d627db5004a
@maverick0701 <madhavanandcp070@gmail.com>
2020-11-28 21:24:27 +0530

    step section animate

--- index.js
+++ index.js
@@ -18,7 +18,13 @@ app.set('layout extractScripts', true);
 
 
 app.set('view engine', 'ejs');
-
+app.use(sassMiddleware({
+    src:'./assets/scss',//from where to pick up css file for compilation
+    dest:'./assets/css',
+    debug:false,//when in production put false
+    outputStyle:'extended',
+    prefix:'/css'//where should look into for css
+}));
 app.use(express.static("./assets"));
 
 app.use(session({
@@ -46,13 +52,7 @@ app.use(session({
 
 
 
-app.use(sassMiddleware({
-    src:'./assets/scss',//from where to pick up css file for compilation
-    dest:'./assets/css',
-    debug:false,//when in production put false
-    outputStyle:'extended',
-    prefix:'/css'//where should look into for css
-}));
+
 
 app.use(passport.initialize());
 app.use(passport.session());
