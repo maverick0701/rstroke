22d2bfd32650aa97611f10e581d065130c06bb1a
@maverick0701 <madhavanandcp070@gmail.com>
2020-12-12 00:28:25 +0530

    added next and prev button fully functional

--- assets/js/form.js
+++ assets/js/form.js
@@ -20,7 +20,50 @@ class formCarousel
             this.currentIndex++;
             this.cont.eq(this.currentIndex).removeClass('setDisplayNone');
             this.cont.eq(this.currentIndex).addClass('con');
+            if(this.currentIndex!=this.len-1)
+            {
+                $('#next').removeAttr('disabled');
+            }
+            if(this.currentIndex==this.len-1)
+            {
+                $('#next').attr('disabled','true');
+            }
+            if(this.currentIndex!=0)
+            {
+                $('#prev').removeAttr('disabled');
+            }
+            if(this.currentIndex==0)
+            {
+                $('#prev').attr('disabled','true');
+            }
+            
+
+        });
+        $('#prev').click((event)=>{
+            event.preventDefault();
+            this.cont.eq(this.currentIndex).removeClass('con');
+            this.cont.eq(this.currentIndex).addClass('setDisplayNone');
+            this.currentIndex--;
+            this.cont.eq(this.currentIndex).removeClass('setDisplayNone');
+            this.cont.eq(this.currentIndex).addClass('con');
+            if(this.currentIndex!=this.len-1)
+            {
+                $('#next').removeAttr('disabled');
+            }
+            if(this.currentIndex==this.len-1)
+            {
+                $('#next').attr('disabled','true');
+            }
+            if(this.currentIndex!=0)
+            {
+                $('#prev').removeAttr('disabled');
+            }
+            if(this.currentIndex==0)
+            {
+                $('#prev').attr('disabled','true');
+            }
         })
+        
 
     }
 
