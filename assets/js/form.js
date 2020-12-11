b2cc55256f0f41ce17ba8372fa4d84d236ecfee3
@maverick0701 <madhavanandcp070@gmail.com>
2020-12-12 00:16:14 +0530

    next added

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,8 +1,28 @@
+
 class formCarousel
 {
-    constructor(len)
+    constructor(lent)
     {
-        let currentIndex=0;
-        let length=this.len;
+        this.currentIndex=0;
+        this.len=lent;
+        this.rotate();
+        this.cont=$('#box #divs');
     }
+    rotate=()=>
+    {
+        // console.log(this);
+        // let pself=this;
+        $('#next').click((event)=>{
+            event.preventDefault();
+            console.log(this);
+            this.cont.eq(this.currentIndex).removeClass('con');
+            this.cont.eq(this.currentIndex).addClass('setDisplayNone');
+            this.currentIndex++;
+            this.cont.eq(this.currentIndex).removeClass('setDisplayNone');
+            this.cont.eq(this.currentIndex).addClass('con');
+        })
+
+    }
+
 }
+
