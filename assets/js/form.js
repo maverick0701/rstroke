8b1a3f7b4354d2f9056ad0d6ee59eca8c0677f15
@maverick0701 <madhavanandcp070@gmail.com>
2020-12-12 11:40:58 +0530

    almost done with the front end

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,4 +1,3 @@
-
 class formCarousel
 {
     constructor(lent)
@@ -7,25 +6,28 @@ class formCarousel
         this.len=lent;
         this.rotate();
         this.cont=$('#box #divs');
+        this.circle=$('#timeLine div');
+        this.circle.eq(this.currentIndex).css({"background-color": "#ffc700"})
     }
     rotate=()=>
     {
-        // console.log(this);
-        // let pself=this;
         $('#next').click((event)=>{
             event.preventDefault();
-            console.log(this);
             this.cont.eq(this.currentIndex).removeClass('con');
             this.cont.eq(this.currentIndex).addClass('setDisplayNone');
+            this.circle.eq(this.currentIndex).css({"background-color": "white"});
             this.currentIndex++;
             this.cont.eq(this.currentIndex).removeClass('setDisplayNone');
             this.cont.eq(this.currentIndex).addClass('con');
+            this.circle.eq(this.currentIndex).css({"background-color": "#ffc700"});
             if(this.currentIndex!=this.len-1)
             {
                 $('#next').removeAttr('disabled');
+                $('#sub').attr('disabled','true');
             }
             if(this.currentIndex==this.len-1)
             {
+                $('#sub').removeAttr('disabled');
                 $('#next').attr('disabled','true');
             }
             if(this.currentIndex!=0)
@@ -36,22 +38,24 @@ class formCarousel
             {
                 $('#prev').attr('disabled','true');
             }
-            
-
         });
         $('#prev').click((event)=>{
             event.preventDefault();
             this.cont.eq(this.currentIndex).removeClass('con');
             this.cont.eq(this.currentIndex).addClass('setDisplayNone');
+            this.circle.eq(this.currentIndex).css({"background-color": "white"})
             this.currentIndex--;
             this.cont.eq(this.currentIndex).removeClass('setDisplayNone');
             this.cont.eq(this.currentIndex).addClass('con');
+            this.circle.eq(this.currentIndex).css({"background-color": "#ffc700"});
             if(this.currentIndex!=this.len-1)
             {
+                $('#sub').attr('disabled','true');
                 $('#next').removeAttr('disabled');
             }
             if(this.currentIndex==this.len-1)
             {
+                $('#sub').removeAttr('disabled');
                 $('#next').attr('disabled','true');
             }
             if(this.currentIndex!=0)
