c53852df6992766af6e9f986251f6bcfad01fa4e
@maverick0701 <madhavanandcp070@gmail.com>
2021-01-23 16:47:52 +0530

    Added exp form

--- controller/users_controller.js
+++ controller/users_controller.js
@@ -1,5 +1,6 @@
 const User = require('../models/user');
 const edu=require('../models/edu');
+const exp=require('../models/exp');
 const Form=require('../models/pdfForm');
 const fs = require('fs');
 const Path = require('path');
@@ -48,6 +49,18 @@ module.exports.update=async function(req,res)
       });
       let user=await User.findById(req.user.id);
       user.education.push(edd.id);
+
+      console.log(user.id);
+      user.save();
+    }
+    else if(key=='experience')
+    {
+      var exx=await exp.create({
+        id:req.user,
+        Past_Experience:req.body.Past_Experience
+      });
+      let user=await User.findById(req.user.id);
+      user.experience.push(exx.id);
 
       console.log(user.id);
       user.save();
